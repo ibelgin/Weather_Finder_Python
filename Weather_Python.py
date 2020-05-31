f423e5e3ca902400d851c08499881f6a51a99e83
Belgin Android <61349423+Belgin-Android@users.noreply.github.com>
2020-05-31 15:50:48 +0530

    Add files via upload

---
+++ Weather_Python.py
@@ -0,0 +1,16 @@
+import requests
+
+city=input("Enter Your City --> ")
+Api_Key = "2606f769271b8d545fe3458b2b72ed9f" # Paste Your API ID Here
+
+final_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format(city,Api_Key)
+
+result = requests.get(final_URL)
+data = result.json()
+
+temprature = data['main']['temp']
+cordinatelon = data['coord']['lon']
+cordinatelat = data['coord']['lat']
+print(temprature," ",cordinatelat," ",cordinatelon)
+
+
